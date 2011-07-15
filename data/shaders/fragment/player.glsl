8c56b795d9615aabc44fe57a1f3ac988b5b04fc5
dfyx <dfyx@wecallit42.de>
2011-07-15 02:10:11 +0200

    Display player identifier

--- data/shaders/fragment/player.glsl
+++ data/shaders/fragment/player.glsl
@@ -1,11 +1,14 @@
 varying vec3 normal;
 varying vec3 texCoord;
 varying vec4 worldPos;
-uniform sampler2D diffuseTexture, specularTexture;
+uniform sampler2D diffuseTexture, specularTexture, identifierTexture;
+uniform vec3 playerColor;
 
 void main()
 {
-    gl_FragData[0] = texture2D(diffuseTexture, gl_TexCoord[0].st);
+    vec4 diffuse = texture2D(diffuseTexture, gl_TexCoord[0].st);
+    vec4 identifier = texture2D(identifierTexture, gl_TexCoord[0].st);
+    gl_FragData[0] = diffuse * (1.0 - identifier.a) + identifier * vec4(playerColor, 1.0) * identifier.a;
     gl_FragData[1] = texture2D(specularTexture, gl_TexCoord[0].st);
     gl_FragData[2] = worldPos;
     gl_FragData[3] = vec4(normalize(normal), 1.0);
